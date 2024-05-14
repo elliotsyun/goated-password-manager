8513bd3518b02a58530c91ccc9acb96311a885f6
dokkaebiyun <elliotyun@vt.edu>
2024-05-14 18:48:15 -0400

    Implemented generate_with_string to hold words together rather than characters

--- generator.py
+++ generator.py
@@ -15,47 +15,44 @@ def generate(length):
     return result
 
 
-def identify_string(input_string):
-    '''Separate an input string based on the types of characters within it'''
-
-    digits = "".join([char for char in input_string if char.isdigit()])
-    letters = "".join([char for char in input_string if char.isalpha()])
-    punctuation = "".join(
-        [char for char in input_string if char in string.punctuation])
-    others = "".join(
-        [char for char in input_string if not char.isdigit() and not char.isalpha() and char not in string.punctuation])
-
-    return digits + letters + punctuation + others
-
-
-def generate_with_string(keywords):
-    '''Generate a string with given keywords, making sure that a string has at
+def generate_with_string(input_string):
+    '''Generate a string with given keywords, separated by whitespace, making sure that a string has at
     least a certain length of DEFAULT_LENGTH'''
 
-    # make keywords a string
-    password_string = identify_string(keywords)
-
-    # make it a list so that we can shuffle it and give the result back as a string
-    pass_list = list(password_string)
-    random.shuffle(pass_list)
-    result = "".join(pass_list)
+    # make keywords a string, make a list
+    keywords = input_string.split()  # list
+    result = ("".join(keywords)).replace(" ", "")  # string
 
     # if the string is less than DEFAULT_LENGTH, then add random characters to the end of it
-    if len(password_string) < DEFAULT_LENGTH:  # 10
-        remaining_length = DEFAULT_LENGTH - len(password_string)  # 4
-        more_password = generate(remaining_length)  # generate 4 random
-        result += more_password
-        random.shuffle(result)
+    if len(result) < DEFAULT_LENGTH:  # 9
 
-        print(result)
+        remaining_length = DEFAULT_LENGTH - len(result)  # 5
+        more_password = list(generate(remaining_length))  # list
+
+        keywords += more_password  # concat the lists
+        random.shuffle(keywords)
+        result = "".join(keywords)
+
         return result
+
+    else:
+        random.shuffle(keywords)
+        result = "".join(keywords)
 
     print(result)
     return result
 
 
-print("This is generate with string\n")
-generate_with_string("abcdef1234!@#$;")
+def regular_password(password):
+    reg_pass = str(password)
 
-print("This is regular generate\n")
+    return reg_pass
+
+
+print("This is generate with string")
+generate_with_string("pen is esss hai rs")
+print("\n")
+
+print("This is regular generate")
 generate(DEFAULT_LENGTH)
+print("\n")
